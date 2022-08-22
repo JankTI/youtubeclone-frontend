a7b6a33769debd3abc2006877d99ea80b65cf2f3
JankTI <1308315453@qq.com>
2022-08-22 17:00:55 +0800

    上传视频

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,28 @@
-import { createStore } from 'vuex'
+import { createStore, Store, useStore as baseUseStore } from 'vuex'
+import { InjectionKey } from 'vue'
+import { User } from '@/api/user'
 
-export default createStore({
+// 声明 State 类型
+export interface State {
+  count: number
+  user: User | null
+}
+
+export const key: InjectionKey<Store<State>> = Symbol('wwe')
+
+export const store = createStore<State>({
   state: {
-  },
-  getters: {
+    count: 0,
+    user: JSON.parse(window.localStorage.getItem('user') || 'null')
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    setUser (state, user: User) {
+      state.user = user
+      window.localStorage.setItem('user', JSON.stringify(state.user))
+    }
   }
 })
+
+export const useStore = () => {
+  return baseUseStore(key)
+}
